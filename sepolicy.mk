e42fc8cd1dc6da4e3a42d3ff00e7b46deb2470ff
Adin Kwok <adin.kwok@carbonrom.org>
2018-01-31 02:39:10 -0500

    Revert "sepolicy: inherit our custom qcom sepolicy"
    This reverts commit 5ed1ee4d18cdf44698550de0c3b14a7d621bbea1.
    
    Change-Id: Ibdb94cd68f0881f3ae0e0066fdf626f8a6d2420e

--- sepolicy.mk
+++ sepolicy.mk
@@ -15,4 +15,4 @@ BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
     device/qcom/sepolicy/private
 
--include device/carbon/sepolicy/qcom/sepolicy.mk
+#-include vendor/carbon/sepolicy/qcom/sepolicy.mk
